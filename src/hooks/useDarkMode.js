4fa6afa049a2fcc0a9de4d5609b647b8140e02dd
sooof <sooof@me.com>
2021-12-02 00:59:00 -0800

    useDarkMode work M2

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,26 +2,26 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import useLocalStorage from "./useLocalStorage";
 
-const useStorage = (initialValue) => {
-    const [darkMode, setDarkMode] = useLocalStorage('darkMode',initialValue)
-    return [darkMode, setDarkMode]
+export const useStorage = (initialValue) => {
+    const [storageValue, setStorageValue] = useLocalStorage('darkMode',initialValue)
+    return [storageValue, setStorageValue]
 }
-export default useStorage;
+// export default useStorage;
 
-const useDarkMode = () => {
-    // const [coinData, setCoinData] = useState([]);
+ const useDarkMode = () => {
+    const [coinData, setCoinData] = useState([]);
     // const [darkMode, setDarkMode] = useState(false);
-    // const [darkMode, setDarkMode] = useStorage(false);
+    const [darkMode, setDarkMode] = useStorage(false);
   
-    // useEffect(() => {
-    //     axios
-    //       .get(
-    //         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-    //       )
-    //       .then(res => setCoinData(res.data))
-    //       .catch(err => console.log(err));
-    //   }, []);
+    useEffect(() => {
+        axios
+          .get(
+            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+          )
+          .then(res => setCoinData(res.data))
+          .catch(err => console.log(err));
+      }, []);
   
-    // return [coinData, darkMode, setDarkMode];
-  }
-//   export default useDarkMode;
+    return [coinData, darkMode, setDarkMode];
+}
+export default useDarkMode ;
