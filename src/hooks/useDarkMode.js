7e0a1d2a41d31ba9e35180309193f866281993fb
sooof <sooof@me.com>
2021-12-02 00:44:44 -0800

    change useDarkMode to useStorage

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,8 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import useLocalStorage from "./useLocalStorage";
-const useDarkMode = (initialValue) => {
-    const [darkMode, setDarkMode] = useLocalStorage('darkmode',initialValue)
 
+const useStorage = (initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode',initialValue)
     return [darkMode, setDarkMode]
 }
-export default useDarkMode;
+export default useStorage;
+
+const useDarkMode = () => {
+    // const [coinData, setCoinData] = useState([]);
+    // const [darkMode, setDarkMode] = useState(false);
+    // const [darkMode, setDarkMode] = useStorage(false);
+  
+    // useEffect(() => {
+    //     axios
+    //       .get(
+    //         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+    //       )
+    //       .then(res => setCoinData(res.data))
+    //       .catch(err => console.log(err));
+    //   }, []);
+  
+    // return [coinData, darkMode, setDarkMode];
+  }
+//   export default useDarkMode;
