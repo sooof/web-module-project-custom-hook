c3f48109972ba81894e890d04892f71d3b95a050
sooof <sooof@me.com>
2021-12-01 18:20:28 -0800

    add hook useDarkMode.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import axios from "axios";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
+import useDarkMode from './hooks/useDarkMode';
+
 import "./style.css";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  // const [coinData, setCoinData] = useState([]);
+  // const [darkMode, setDarkMode] = useState(false);
+  const [coinData, darkMode, setDarkMode] = useDarkMode();
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+  //     )
+  //     .then(res => setCoinData(res.data))
+  //     .catch(err => console.log(err));
+  // }, []);
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
